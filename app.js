e51e8f23c0d4972a6377a9c28a036fb3e6b525c1
Landon <donovandelint.09@hotmail.com>
2025-10-20 22:09:42 -0600

    con derek

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const port = 8080;
 //Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Home Page con Tadeo");
-});
-
 app.get("/hola-mundo", (req, res) => {
   res.send("Hola mundo con Tadeo");
+});
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
