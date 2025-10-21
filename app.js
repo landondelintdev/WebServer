f926b862b953e24f4e6bc9f89a21ff68733a3cff
Landon <donovandelint.09@hotmail.com>
2025-10-21 13:40:25 -0600

    TemplateNuevo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,15 +9,23 @@ const app = express();
 const port = 8080;
 
 // Middleware: servir contenido estático
-app.use(express.static("public"));
+app.use(express.static(join(__dirname, "public")));
 
-app.get("/hola-mundo", (req, res) => {
-  res.send("Hola mundo con Tadeo");
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/generic", (req, res) => {
+  res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", (req, res) => {
+  res.sendFile(__dirname + "/public/elements.html");
 });
 
 // Ruta comodín (404)
 app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/404");
 });
 
 app.listen(port, () => {
