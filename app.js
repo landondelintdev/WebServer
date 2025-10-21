7eef34c24ab93b59b4b85ad13bcf96b1a74607da
Landon <donovandelint.09@hotmail.com>
2025-10-21 11:17:29 -0600

    correcion

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8080;
 
-//middleware
-//Servir contenido estatico
+// Middleware: servir contenido estático
 app.use(express.static("public"));
 
 app.get("/hola-mundo", (req, res) => {
   res.send("Hola mundo con Tadeo");
 });
 
+// Ruta comodín (404)
 app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
